7f57a9dd9267e7d6404dd031e9bcf0a7dde79658
Emre Elbir <59312457+emreelbir@users.noreply.github.com>
2020-12-18 05:18:02 +0100

    Add project files.

---
+++ Log4net/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+[assembly: log4net.Config.XmlConfigurator(Watch =true)]
+
+namespace Log4net
+{
+    class Program
+    {
+        private static readonly log4net.ILog log = LogHelper.GetLogger();//(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+
+            log.Error("This is my error massage ! ");
+
+            Console.ReadLine();
+        }
+    }
+}
